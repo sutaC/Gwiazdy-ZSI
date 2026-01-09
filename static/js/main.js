bb2e9662441c68ed9a01a4c25c407526c4bdd26c
sutaC <catus.pl@gmail.com>
2026-01-09 21:37:22 +0100

    Help Wanted message

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,22 @@
 // @ts-check
+const dialogHelpWanted = /** @type {HTMLDialogElement} */ (
+    document.getElementById("dialogHelpWanted")
+);
+
+dialogHelpWanted?.addEventListener(
+    "close",
+    () => {
+        localStorage.setItem("helpwantedclosed", "true");
+    },
+    { once: true }
+);
+document.getElementById("btnHelpWantedClose")?.addEventListener("click", () => {
+    dialogHelpWanted?.close();
+});
+document.getElementById("btnHelpWantedOpen")?.addEventListener("click", () => {
+    dialogHelpWanted?.show();
+});
 
 if (!Boolean(JSON.parse(localStorage.getItem("helpwantedclosed") ?? "false"))) {
-    const dialogHelpWanted = /** @type {HTMLDialogElement} */ (
-        document.getElementById("dialogHelpWanted")
-    );
-    dialogHelpWanted.addEventListener("close", () => {
-        localStorage.setItem("helpwantedclosed", "true");
-    });
-    document
-        .getElementById("btnHelpWantedClose")
-        ?.addEventListener("click", () => {
-            dialogHelpWanted.close();
-        });
-    setTimeout(() => dialogHelpWanted.show(), 1000);
+    setTimeout(() => dialogHelpWanted?.show(), 1000);
 }
